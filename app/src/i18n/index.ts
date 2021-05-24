e19680ae926f6b54d6b9f9d62e0b94619a2f40a5
Max I <ismak89@gmail.com>
2021-05-24 19:23:34 +0300

    Added i18next

---
+++ app/src/i18n/index.ts
@@ -0,0 +1,19 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+
+const translationEN = require("./ru.json");
+
+i18n
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en: {
+				translation: translationEN
+			},
+		},
+		lng: "en",
+		fallbackLng: "en",
+		interpolation: {
+			escapeValue: false
+		}
+	});
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./i18n";
 
 ReactDOM.render(
   <App />,
-  document.getElementById("root")
+  document.getElementById("appMountPoint")
 );
-
