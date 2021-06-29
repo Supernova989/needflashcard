5b2e4ed766aec042ad454ffa05ffe2c540df6c01
Max Isaev <ismak@eml.cc>
2021-06-29 20:47:30 +0300

    Redirecting from /login when authorized

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
-import React, { lazy } from "react";
-import { Switch, Route, RouteComponentProps } from "react-router-dom";
+import React, { FC, lazy } from "react";
+import { Switch, Route, RouteComponentProps, Redirect } from "react-router-dom";
 import { ROUTES } from "./routes";
 import BasicLayout from "./layouts/BasicLayout";
 import NoUserLayout from "./layouts/NoUserLayout";
 import PrivateRoute from "./components/PrivateRoute";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 const IndexPage = lazy(() => import("./pages/IndexPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
@@ -13,18 +15,24 @@ const ProfilePage = lazy(() => import("./pages/ProfilePage"));
 const AboutPage = lazy(() => import("./pages/AboutPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
-function App() {
+const App: FC = () => {
+  const isAuthorized = useSelector((state: RootState) => state.auth.token);
+
   return (
     <>
       <Switch>
         <Route
           path={ROUTES.LOGIN}
-          component={(props: RouteComponentProps<{}, {}, { referer?: string }>) => (
-            <NoUserLayout>
-              <LoginPage referer={props?.location?.state?.referer} />
-            </NoUserLayout>
-          )}
+          component={(props: RouteComponentProps<{}, {}, { referer?: string }>) => {
+            const page = (
+              <NoUserLayout>
+                <LoginPage referer={props?.location?.state?.referer} />
+              </NoUserLayout>
+            );
+            return !isAuthorized ? page : <Redirect to={ROUTES.INDEX} />;
+          }}
         />
+
         <PrivateRoute
           path={ROUTES.INDEX}
           exact
@@ -81,6 +89,6 @@ function App() {
       </Switch>
     </>
   );
-}
+};
 
 export default App;
